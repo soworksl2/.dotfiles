dd0fd85bef66b39aab67494b26223a916276a076
soworksl2 <jimy.waner11@gmail.com>
2024-11-15 23:26:36 -0400

    [user_script] screeshot tool added TODOS and delete temp img
    - deleting the temp image saved in tmp when finished
    - TODO for the future bb

--- user_scripts/user_scripts/screenshot_menu.sh
+++ user_scripts/user_scripts/screenshot_menu.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#prepare screenshots directory from scripts
-mkdir -p "$HOME/images/screenshots/"
+#TODO: Make this script launch a notification when the screenshot is done
 
+
+#TODO: instead of (NO) use _NO to make easy to manipulate through coreutils file managment tools
 function get_next_filename_nu {
     if [ "$2" -gt 150 ]; then
         echo "error too much recursion file No. (${2})"
@@ -27,28 +28,29 @@ function get_next_filename {
         return
     fi
 
-
     echo "$(get_next_filename_nu $1 1)"
 }
 
 
+#make screenshots directory available
+mkdir -p "$HOME/images/screenshots/"
+
+
+#Screen options
 fullopt="fullscreen screenshot"
 partopt="part screenshot"
 windowopt="windows screenshot"
 
-#select option
-opt=$(echo -e "$fullopt\n$partopt\n$windowopt" | wofi -d)
-
-if [ $? -ne 0 ]; then
-    echo "no opts selected" >&2
+screen_opt=$(echo -e "$fullopt\n$partopt\n$windowopt" | wofi --dmenu)
+if test "$?" -ne 0; then
+    echo "no screen option selected!" >&2
     exit 1
 fi
 
 today=$(date +'%D' | sed s#/#_#g)
-temp_last_screenshot="/tmp/${today}_soworksl_last_screenshot.png"
+temp_last_screenshot="/tmp/${today}_soworksl_last_screenshot.png" #TODO: Should I save in /tmp it is ok but is in ram and is a img that I am saving?
 
-case "$opt" in
-
+case "$screen_opt" in
     "$fullopt")
         sleep 1 #wait for the dmenu disapears
         grim "$temp_last_screenshot"
@@ -65,29 +67,31 @@ case "$opt" in
         ;;
     
     *)
-        echo error >&2
+        echo "screen option (${screen_opt}) is not recognized" >&2
         exit 1
         ;;
 esac
 
-if [ $? -ne 0 ]; then
+if test "$?" -ne 0; then
+    rm --force "${temp_last_screenshot}"
     echo "fail in the screenshot" >&2
     exit 1
 fi
+
 
 saveopt="save in images"
 copyopt="copy to the clipboard"
 sandcopt="save and copy to the clipboard"
 
-opt=$(echo -e "$saveopt\n$copyopt\n$sandcopt" | wofi -d)
-
+#The selection of save is done here to not annoy user while taking picture, that is because
+#he should be in a hurry.
+save_opt=$(echo -e "$saveopt\n$copyopt\n$sandcopt" | wofi -d)
 if [ $? -ne 0 ]; then
-    echo "no opts selected" >&2
+    echo "no save option selected!" >&2
     exit 1
 fi
  
-case "$opt" in
-
+case "$save_opt" in
     "$saveopt")
         mv "$temp_last_screenshot" "$HOME/images/screenshots/$(get_next_filename "${today}_img").png"
         echo "image saved"
@@ -106,8 +110,9 @@ case "$opt" in
         ;;
 
     *)
-        echo "error" >&2
+        echo "save_opt (${save_opt}) not regognized" >&2
         exit 1
         ;;
-
 esac
+
+rm --force "${temp_last_screenshot}"
