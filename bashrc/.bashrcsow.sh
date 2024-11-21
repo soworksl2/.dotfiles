9702d16bbe014339c54b339faca3e7ef85aabe12
soworksl2 <jimy.waner11@gmail.com>
2024-11-21 18:10:32 -0400

    adding XDG_BIN_HOME to bash

--- bashrc/.bashrcsow.sh
+++ bashrc/.bashrcsow.sh
@@ -2,6 +2,11 @@
 PS1="\n[\w]\n[\u]--> "
 
 
+#adding some xdg stuff
+export XDG_BIN_HOME="$HOME/.local/bin"
+export PATH="${XDG_BIN_HOME}:${PATH}"
+
+
 #Editors
 export EDITOR="nvim"
 export VISUAL="nvim"
